38217509bbad06e117935fd8d5d96b609af17202
mittalrahul074 <59787115+mittalrahul074@users.noreply.github.com>
2021-01-31 20:12:03 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    while(a!=42)
+    {
+        cout<<a<<endl;
+        std::cin >> a;
+    }
+	// your code goes here
+	return 0;
+}
